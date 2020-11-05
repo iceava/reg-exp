e8869a49fe0d55b12dd1f2d47156390beb66e039
Ivan Dovbenco <mr.phenomen.pro@gmail.com>
2020-11-05 15:39:10 +0200

    homeWork-7

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+function replaceCSSComments(str) {
+    return str.replace(/\/\*.*?\*\/\s?/g, '');
+  }
+
+
+
+
+
+function replaceHTMLComments(str) {
+    return str.replace(/\<\!\-\-*.*?\-\-\>\s*/g, '').trim();
+  }
+
+
+function validateFileType(str) {
+    return (/.+(\.jpg|\.jpeg|\.png)$/).test(str);
+  }
+
+
+
+function checkYear(str) {
+  return (/^(19\d\d|20\d\d|2100)$/).test(str);
+}
+
+
+
+function checkEmail(str) {
+    return (/^[a-zA-z]+\W?[a-zA-z]+@[a-zA-z\.]+\.[a-z]{2,3}$/).test(str);
+  }
+
+
+
+function checkDomainUrl(str) {
+    return (/^(http:|https:)\/\/(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,5}?$/).test(str);
+  }
+
+
+  
+
+function createLinksFromDomains(str) {
+    let reg = new RegExp('(http:|https:)\\/\\/(?!:\\/\\/)([a-zA-Z0-9-_]+\\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\\.[a-zA-Z]{2,5}', 'gi');
+  
+    str.match(reg).forEach((item) => str = str.replace(item, `<a href="${item}" target="_blank">${item.replace(/^(http:|https:)\/\//gi, '')}</a>`));
+  
+    return str;
+  }
